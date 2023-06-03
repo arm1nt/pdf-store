95d8db8ba1593048dd922c77ce10de038bd23083
Armin <66900830+ArminNT@users.noreply.github.com>
2023-06-03 12:16:43 +0200

    feat: Added basic project structure

---
+++ backend/pdf-store/src/main/java/com/arm1nt/pdfstore/PdfStoreApplication.java
@@ -0,0 +1,13 @@
+package com.arm1nt.pdfstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfStoreApplication.class, args);
+    }
+
+}
---
+++ backend/pdf-store/src/test/java/com/arm1nt/pdfstore/PdfStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arm1nt.pdfstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
